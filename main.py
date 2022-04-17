5f4e03a9e64e882cdc0732c0bd5addb76364f03e
Harsh Singh <44111692+harshzenith50@users.noreply.github.com>
2022-04-17 15:51:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,110 @@
+from my_functions import *
+
+source = 'test_video.MOV'
+
+save_video = True  # want to save video? (when video as source)
+show_video = True  # set true when using video file
+save_img = False  # set true when using only image file to save the image
+# when using image as input, lower the threshold value of image classification
+
+# saveing video as output
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, frame_size)
+
+cap = cv2.VideoCapture(source)
+while cap.isOpened():
+    ret, frame = cap.read()  # read([, image]) -> retval(true or false), image
+    if ret == True:
+        frame = cv2.resize(frame, frame_size)  # resizing image
+        orifinal_frame = frame.copy()
+        frame, results = object_detection(frame)
+        rider_list = []
+        head_list = []
+        number_list = []
+        for result in results:
+            x1, y1, x2, y2, cnf, clas = result
+            if clas == 0:
+                rider_list.append(result)
+            elif clas == 1:
+                head_list.append(result)
+            elif clas == 2:
+                number_list.append(result)
+
+        for rdr in rider_list:
+            time_stamp = str(time.time())
+            x1r, y1r, x2r, y2r, cnfr, clasr = rdr
+            for hd in head_list:
+                x1h, y1h, x2h, y2h, cnfh, clash = hd
+                if inside_box([x1r, y1r, x2r, y2r], [x1h, y1h, x2h, y2h]):  # if this head inside this rider bbox
+                    try:
+                        head_img = orifinal_frame[y1h:y2h, x1h:x2h]
+                        helmet_present = img_classify(head_img)
+                    except:
+                        helmet_present[0] = None
+
+                    if helmet_present[0] == True:  # if helmet present
+                        frame = cv2.rectangle(frame, (x1h, y1h), (x2h, y2h), (0, 255, 0), 1)
+                        frame = cv2.putText(frame, f'{round(helmet_present[1], 1)}', (x1h, y1h + 40),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    elif helmet_present[0] is None:  # Poor prediction
+                        frame = cv2.rectangle(frame, (x1h, y1h), (x2h, y2h), (0, 255, 255), 1)
+                        frame = cv2.putText(frame, f'{round(helmet_present[1], 1)}', (x1h, y1h),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    elif helmet_present[0] == False:  # if helmet absent
+                        frame = cv2.rectangle(frame, (x1h, y1h), (x2h, y2h), (0, 0, 255), 1)
+                        frame = cv2.putText(frame, f'{round(helmet_present[1], 1)}', (x1h, y1h + 40),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                        try:
+                            cv2.imwrite(f'riders_pictures/{time_stamp}.jpg', frame[y1r:y2r, x1r:x2r],[int(cv2.IMWRITE_JPEG_QUALITY), 100])
+                        except:
+                            print('could not save rider')
+
+                        for num in number_list:
+                            x1_num, y1_num, x2_num, y2_num, conf_num, clas_num = num
+                            if inside_box([x1r, y1r, x2r, y2r], [x1_num, y1_num, x2_num, y2_num]):
+                                try:
+                                    num_img = orifinal_frame[y1_num:y2_num, x1_num:x2_num]
+                                    cv2.imwrite(f'number_plates/{time_stamp}_{conf_num}.jpg', num_img,[int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+                                except:
+                                    print('could not save number plate')
+
+        if save_video:  # save video
+            out.write(frame)
+        if save_img:  # save img
+            cv2.imwrite('saved_frame.jpg', frame)
+        if show_video:  # show video
+            frame = cv2.resize(frame, (900, 450))  # resizing to fit in screen
+            cv2.imshow('Frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+print('Execution completed')
+
+import requests
+import os
+import time
+from pprint import pprint
+a = []
+path = "C:\\Users\\Harsh\\motorcycle_license_plate-main\\number_plates"
+regions = ['in'] # Change to your country
+for filename in os.listdir(path):
+   with open(os.path.join(path, filename), 'rb') as f:
+    response = requests.post(
+        'https://api.platerecognizer.com/v1/plate-reader/',
+        data=dict(regions=regions),  # Optional
+        files=dict(upload=f),
+        headers={'Authorization': 'Token 4bd7132459fe03bf5cf13eb1f0d58dd26f76c4bf'})
+    r = response.json()
+#print(r)
+#print("License Plate: ")
+    a.append(r['results'][0]['plate'])
+    time.sleep(1.02)
+for i in set(a):
+    print(f"License Plate: {i}")
---
+++ plate_recognition.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function
+
+import argparse
+import collections
+import csv
+import json
+import time
+from collections import OrderedDict
+from pathlib import Path
+
+import requests
+
+
+def parse_arguments(args_hook=lambda _: _):
+    parser = argparse.ArgumentParser(
+        description=
+        'Read license plates from images and output the result as JSON or CSV.',
+        epilog="""Examples:'
+Process images from a folder: python plate_recognition.py -a 29ee3c4e83e51723e22d0eaa284502947d6df535 /path/to/vehicle-*.jpg
+Use the Snapshot SDK instead of the Cloud Api: python plate_recognition.py -s http://localhost:8080 /path/to/vehicle-*.jpg
+Specify Camera ID and/or two Regions: plate_recognition.py -a 29ee3c4e83e51723e22d0eaa284502947d6df535 --camera-id Camera1 -r us-ca -r th-37 /path/to/vehicle-*.jpg""",
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-a', '--api-key', help='29ee3c4e83e51723e22d0eaa284502947d6df535', required=False)
+    parser.add_argument(
+        '-r',
+        '--regions',
+        help='Match the license plate pattern of a specific region',
+        required=False,
+        action="append")
+    parser.add_argument(
+        '-s',
+        '--sdk-url',
+        help="Url to self hosted sdk  For example, http://localhost:8080",
+        required=False)
+    parser.add_argument('--camera-id',
+                        help="Name of the source camera.",
+                        required=False)
+    parser.add_argument('files', nargs='+', help='Path to vehicle images')
+    args_hook(parser)
+    args = parser.parse_args()
+    if not args.sdk_url and not args.api_key:
+        raise Exception('api-key is required')
+    return args
+
+
+_session = None
+
+
+def recognition_api(fp,
+                    regions=[],
+                    api_key='29ee3c4e83e51723e22d0eaa284502947d6df535',
+                    sdk_url=None,
+                    config={},
+                    camera_id=None,
+                    timestamp=None,
+                    mmc=None,
+                    exit_on_error=True):
+    global _session
+    data = dict(regions=regions, config=json.dumps(config))
+    if camera_id:
+        data['camera_id'] = camera_id
+    if mmc:
+        data['mmc'] = mmc
+    if timestamp:
+        data['timestamp'] = timestamp
+    response = None
+    if sdk_url:
+        fp.seek(0)
+        response = requests.post(sdk_url + '/v1/plate-reader/',
+                                 files=dict(upload=fp),
+                                 data=data)
+    else:
+        if not _session:
+            _session = requests.Session()
+            _session.headers.update({'Authorization': 'Token ' + api_key})
+        for _ in range(3):
+            fp.seek(0)
+            response = _session.post(
+                'https://api.platerecognizer.com/v1/plate-reader/',
+                files=dict(upload=fp),
+                data=data)
+            if response.status_code == 429:  # Max calls per second reached
+                time.sleep(1)
+            else:
+                break
+
+    if response is None:
+        return {}
+    if response.status_code < 200 or response.status_code > 300:
+        print(response.text)
+        if exit_on_error:
+            exit(1)
+    return response.json(object_pairs_hook=OrderedDict)
+
+
+def flatten_dict(d, parent_key='', sep='_'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            if isinstance(v, list):
+                items.append((new_key, json.dumps(v)))
+            else:
+                items.append((new_key, v))
+    return dict(items)
+
+
+def flatten(result):
+    plates = result['results']
+    del result['results']
+    del result['usage']
+    if not plates:
+        return result
+    for plate in plates:
+        data = result.copy()
+        data.update(flatten_dict(plate))
+    return data
+
+
+def save_results(results, args):
+    path = Path(args.output_file)
+    if not path.parent.exists():
+        print('%s does not exist' % path)
+        return
+    if not results:
+        return
+    if args.format == 'json':
+        with open(path, 'w') as fp:
+            json.dump(results, fp)
+    elif args.format == 'csv':
+        fieldnames = []
+        for result in results[:10]:
+            candidate = flatten(result.copy()).keys()
+            if len(fieldnames) < len(candidate):
+                fieldnames = candidate
+        with open(path, 'w') as fp:
+            writer = csv.DictWriter(fp, fieldnames=fieldnames)
+            writer.writeheader()
+            for result in results:
+                writer.writerow(flatten(result))
+
+
+def custom_args(parser):
+    parser.epilog += """
+Specify additional engine configuration: plate_recognition.py -a MY_API_KEY --engine-config \'{"region":"strict"}\' /path/to/vehicle-*.jpg
+Specify an output file and format for the results: plate_recognition.py -a 29ee3c4e83e51723e22d0eaa284502947d6df535 -o data.csv --format csv /path/to/vehicle-*.jpg
+Enable Make Model and Color prediction: plate_recognition.py -a 29ee3c4e83e51723e22d0eaa284502947d6df535 --mmc /path/to/vehicle-*.jpg"""
+
+    parser.add_argument('--engine-config', help='Engine configuration.')
+    parser.add_argument('-o', '--output-file', help='Save result to file.')
+    parser.add_argument('--format',
+                        help='Format of the result.',
+                        default='json',
+                        choices='json csv'.split())
+    parser.add_argument(
+        '--mmc',
+        action='store_true',
+        help='Predict vehicle make and model. Only available to paying users.')
+
+
+def main():
+    args = parse_arguments(custom_args)
+    paths = args.files
+
+    results = []
+    engine_config = {}
+    if args.engine_config:
+        try:
+            engine_config = json.loads(args.engine_config)
+        except json.JSONDecodeError as e:
+            print(e)
+            return
+
+    for path in paths:
+        with open(path, 'rb') as fp:
+            api_res = recognition_api(fp,
+                                      args.regions,
+                                      args.api_key,
+                                      args.sdk_url,
+                                      config=engine_config,
+                                      camera_id=args.camera_id,
+                                      mmc=args.mmc)
+
+        results.append(api_res)
+    if args.output_file:
+        save_results(results, args)
+    else:
+        print(json.dumps(results, indent=2))
+
+
+if __name__ == '__main__':
+    main()
